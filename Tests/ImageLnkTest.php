96b526fc50dd897c7c6984440ec527b03c26e79d
Takayama Fumihiko <tekezo@pqrs.org>
2011-05-30 08:40:48 +0900

    added instagram to Engine

--- Tests/ImageLnkTest.php
+++ Tests/ImageLnkTest.php
@@ -343,4 +343,14 @@ class ImageLnkTest extends PHPUnit_Framework_TestCase {
       );
     $this->check_response($url, $title, $imageurls);
   }
+
+  // ======================================================================
+  function test_instagram1() {
+    $url = 'http://instagr.am/p/E6VjC/';
+    $title = 'yfryer\'s photo: &ldquo;おはよう&rdquo;';
+    $imageurls = array(
+      'http://images.instagram.com/media/2011/05/29/9c4d66f169a24764961977326e2fc1cf_7.jpg',
+      );
+    $this->check_response($url, $title, $imageurls);
+  }
 }
---
+++ lib/Engine/instagram.php
@@ -0,0 +1,28 @@
+<?php //-*- Mode: php; indent-tabs-mode: nil; -*-
+
+class ImageLnkEngine_instagram {
+
+  const sitename = 'http://instagr.am/';
+
+  public static function handle($url) {
+    if (! preg_match('/^http:\/\/instagr\.am\/p\//', $url)) {
+      return FALSE;
+    }
+
+    // ----------------------------------------
+    $data = ImageLnkCache::get($url);
+    $html = $data['data'];
+
+    $response = new ImageLnkResponse();
+    $response->setReferer($url);
+
+    ImageLnkHelper::setResponseFromOpenGraph($response, $html);
+
+    if (preg_match('/<div class="caption">(.*?)<\/div>/s', $html, $matches)) {
+      $response->setTitle($response->getTitle() . ': ' . trim($matches[1]));
+    }
+
+    return $response;
+  }
+}
+ImageLnkEngine::push('ImageLnkEngine_instagram');
