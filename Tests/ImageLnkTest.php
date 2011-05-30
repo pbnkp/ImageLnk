94c5dec581b2718529d3b02eb3433062392346aa
Takayama Fumihiko <tekezo@pqrs.org>
2011-05-30 12:02:40 +0900

    added niconico to Engine

--- Tests/ImageLnkTest.php
+++ Tests/ImageLnkTest.php
@@ -353,4 +353,14 @@ class ImageLnkTest extends PHPUnit_Framework_TestCase {
       );
     $this->check_response($url, $title, $imageurls);
   }
+
+  // ======================================================================
+  function test_niconico1() {
+    $url = 'http://video.niconico.com/watch/http://www.nicovideo.jp/watch/sm11187442';
+    $title = '【弾いてみた】　ウルトラの奇跡　【 ○（ 0|0）o 】';
+    $imageurls = array(
+      'http://tn-skr3.smilevideo.jp/smile?i=11187442',
+      );
+    $this->check_response($url, $title, $imageurls);
+  }
 }
---
+++ lib/Engine/niconico.php
@@ -0,0 +1,23 @@
+<?php //-*- Mode: php; indent-tabs-mode: nil; -*-
+
+class ImageLnkEngine_niconico {
+  const sitename = 'http://www.niconico.com/';
+
+  public static function handle($url) {
+    if (! preg_match('/^http:\/\/video\.niconico\.com\/watch\//', $url)) {
+      return FALSE;
+    }
+
+    // ----------------------------------------
+    $data = ImageLnkCache::get($url);
+    $html = $data['data'];
+
+    $response = new ImageLnkResponse();
+    $response->setReferer($url);
+
+    ImageLnkHelper::setResponseFromOpenGraph($response, $html);
+
+    return $response;
+  }
+}
+ImageLnkEngine::push('ImageLnkEngine_niconico');
