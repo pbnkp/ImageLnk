1ec6fa92f2455e7e46fc727411ba4d047f59a6d4
Takayama Fumihiko <tekezo@pqrs.org>
2011-06-01 17:51:05 +0900

    added engadget_jp to Engine

--- Tests/ImageLnkTest.php
+++ Tests/ImageLnkTest.php
@@ -393,4 +393,23 @@ class ImageLnkTest extends PHPUnit_Framework_TestCase {
       );
     $this->check_response($url, $title, $imageurls);
   }
+
+  // ======================================================================
+  function test_engadget_jp1() {
+    $url = 'http://japanese.engadget.com/photos/asus-eee-pad-memo-3d-memic-hands-on/4173481/';
+    $title = 'Engadget Japanese: Asus Eee Pad MeMO 3D / MeMIC hands on';
+    $imageurls = array(
+      'http://www.blogcdn.com/japanese.engadget.com/media/2011/05/asuseeepadmemohandsoncomputex1103-1306749954.jpg',
+      );
+    $this->check_response($url, $title, $imageurls);
+  }
+
+  function test_engadget_jp2() {
+    $url = 'http://japanese.engadget.com/photos/memorex-gaming-peripherals-e3-2011/4179706/';
+    $title = 'Engadget Japanese: Memorex gaming Peripherals (E3 2011)';
+    $imageurls = array(
+      'http://www.blogcdn.com/japanese.engadget.com/media/2011/06/3dsgameselector.jpg',
+      );
+    $this->check_response($url, $title, $imageurls);
+  }
 }
---
+++ lib/Engine/engadget.jp.php
@@ -0,0 +1,40 @@
+<?php //-*- Mode: php; indent-tabs-mode: nil; -*-
+
+class ImageLnkEngine_engadget_jp {
+  const language = 'Japanese';
+  const sitename = 'http://japanese.engadget.com/';
+
+  public static function handle($url) {
+    if (! preg_match('/^http:\/\/japanese.engadget\.com\/photos\//', $url)) {
+      return FALSE;
+    }
+
+    // ----------------------------------------
+    $data = ImageLnkCache::get($url);
+    $html = $data['data'];
+
+    $response = new ImageLnkResponse();
+    $response->setReferer($url);
+
+    $title = ImageLnkHelper::getTitle($html);
+    if ($title !== FALSE) {
+      $response->setTitle($title);
+    }
+
+    if (preg_match('/<p class="breadcrumb">.*?<em><a .*?>(.*?)<\/a>/', $html, $matches)) {
+      $response->setTitle($response->getTitle() . ': ' . $matches[1]);
+    }
+
+    if (preg_match('/<div class="photo-body">(.*?)<\/div>/', $html, $matches)) {
+      foreach (ImageLnkHelper::scanSingleTag('img', $matches[1]) as $img) {
+        if (preg_match('/ src="(.+?)"/', $img, $m)) {
+          $response->addImageURL($m[1]);
+          break;
+        }
+      }
+    }
+
+    return $response;
+  }
+}
+ImageLnkEngine::push('ImageLnkEngine_engadget_jp');
