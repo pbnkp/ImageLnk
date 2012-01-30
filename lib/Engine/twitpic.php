03df8822e7cfd26c518d79922c5287fa370d6885
Takayama Fumihiko <tekezo@pqrs.org>
2012-01-30 02:12:21 +0000

    change title on twitpic

--- lib/Engine/twitpic.php
+++ lib/Engine/twitpic.php
@@ -23,12 +23,10 @@ class ImageLnkEngine_twitpic {
     $response = new ImageLnkResponse();
     $response->setReferer($url);
 
+    $response->setTitle(ImageLnkHelper::getTitle($html));
     foreach (ImageLnkHelper::scanSingleTag('img', $html) as $img) {
       if (preg_match('% src="(.+?/full/.+?)"%s', $img, $m)) {
         $response->addImageURL($m[1]);
-        if (preg_match('/alt="(.+?)"/s', $img, $m)) {
-          $response->setTitle($m[1]);
-        }
         break;
       }
     }
